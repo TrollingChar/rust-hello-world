71ec46ca5af87b1e3e127a6746f2380703a432db
trollingchar <dmitriyrodin0@gmail.com>
2021-01-06 18:24:46 +0300

    hello world

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,8 @@ type HighFns = crate::high::Fns<Box<dyn FnOnce(&mut String)>>;
 
 
 fn main() {
-    let mut s: String = "Федя".to_owned();
-    let mut h = High::new(HighFns { nop: || Box::new(|s| s.push_str(" лох")) });
+    let mut s: String = "hello".to_owned();
+    let mut h = High::new(HighFns { nop: || Box::new(|s| s.push_str(" world!")) });
     let f = h.update();
     f(&mut s);
     println!("{}", s);
